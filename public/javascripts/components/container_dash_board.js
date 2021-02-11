8b50c4906557b34cdb19fc8e66adcb74ce97672a
Lucas <lucas.larnicol@gmail.com>
2021-02-11 21:04:44 +0100

    adjustement in container component

--- public/javascripts/components/container_dash_board.js
+++ public/javascripts/components/container_dash_board.js
@@ -1,17 +1,24 @@
+import dashboard from './dashboard.js'
 
 const container = {
     props: ['props'],
     template: `
-        <main 
-            class="row valign-wrapper transiAll"
-            :class="{'fixedHeight100': props.isLarge, 'darkModeColor': props.isDarkMode}"
+        <section 
+            class="row transiAll"
+            style="position: relative; overflow: hidden;"
+            :class="{'fixedHeight100': props.isLarge, 'darkModeColor': props.darkMode}"
         
         >
-            <article class="col s12 m8 offset-m2 borderRadius borderTopLeft boxShadow fixedHeight80">
+            <figure style='position: absolute; top:0; left: -10%; width: 50%;'><img class="responsive-img" src="/images/bulle0.svg"/></figure>
+            <figure style='position: absolute; top: -30%; left: 60%; width: 50%;'><img class="responsive-img" src="/images/bulle1.svg"/></figure>
 
-            </article>
-        </main>
-    `
+            <dashboard :props="props"></dashboard>
+        </section>
+    `,
+
+    components: {
+        dashboard: dashboard
+    }
 }
 
 export default container
