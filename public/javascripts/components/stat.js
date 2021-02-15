8c0e63d7b9447acef775942de0444cf0de149efc
LarnicolLucas <75536130+LarnicolLucas@users.noreply.github.com>
2021-02-15 16:08:42 +0100

    Add files via upload
    Add mixin / component graph

---
+++ public/javascripts/components/stat.js
@@ -0,0 +1,15 @@
+import polygraph from './stat/polygraph.js'
+
+const stat = {
+	
+	template: {
+		
+		<svg width="200" height="200">
+			<polygraph :props="props"></polygraph>
+		</svg>
+	},
+	components: {
+		polygraph: polygraph,
+	}
+	
+}
