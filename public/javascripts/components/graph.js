259b9a5d133b171519b666926e0a5c31b10a29d6
Lucas <lucas.larnicol@gmail.com>
2021-02-25 11:14:34 +0100

    add some functionnal in graph

--- public/javascripts/components/graph.js
+++ public/javascripts/components/graph.js
@@ -21,9 +21,11 @@ const graph = {
         points: function(){
             const list_all_geste_by_inter = this.createNewListGeste(this.props.model.interventions);
 
-            let list = this.selectGesteById(list_all_geste_by_inter, this.id_item, this.id_geste);
+            const list = this.selectGesteById(list_all_geste_by_inter, this.id_item, this.id_geste);
 
-            return this.convertListToCoordonate(list)
+            const list_coordonates = this.convertListToCoordonate(list);
+
+            return list_coordonates
         },
 
         size: function(){
@@ -54,6 +56,22 @@ const graph = {
             return array_date.reverse()
         },
 
+        findMaxAndMin: function(list_of_object){
+
+            let array_of_numbers = list_of_object.map(el=> el.date_num);
+            let max = array_of_numbers.reduce((acc, cur)=> Math.max(acc, cur));
+            let min = array_of_numbers.reduce((acc, cur)=> Math.min(acc, cur));  
+            
+            return {
+                max: max,
+                min: min
+            }
+        },
+
+        addNumericDateToListObject: function(list_of_object){
+            return list_of_object.map(el => Object.assign(el, {date_num: (new Date(this.convertDate(el.date)[0], this.convertDate(el.date)[1], this.convertDate(el.date)[2] )).getTime()}));
+        },
+
         convertListToCoordonate: function(list){
 
             if(list.length < 2){
@@ -62,10 +80,11 @@ const graph = {
 
             } else {
 
-                let list_with_num_date = list.map(el => Object.assign(el, {date_num: (new Date(this.convertDate(el.date)[0], this.convertDate(el.date)[1], this.convertDate(el.date)[2] )).getTime()}));
-                let list_date = list_with_num_date.map(el=> el.date_num);
-                let max_x = list_date.reduce((acc, cur)=> Math.max(acc, cur));
-                let min_x = list_date.reduce((acc, cur)=> Math.min(acc, cur));
+                let list_with_num_date = this.addNumericDateToListObject(list);
+                const max_min = this.findMaxAndMin(list_with_num_date);
+                const max_x = max_min.max;
+                const min_x = max_min.min;
+
                 let time = max_x - min_x;
                     
                 return list_with_num_date.map(el => [(((el.date_num - min_x) / time)*100).toFixed(0) * 1,  (el.maitrise / this.props.model.echelle_de_notation) * 100])
