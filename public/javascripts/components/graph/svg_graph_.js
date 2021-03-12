418801a8e45389ed548fd3fd4fdf826fbadb8cd3
Lucas <lucas.larnicol@gmail.com>
2021-03-12 10:37:30 +0100

    save graph with line

---
+++ public/javascripts/components/graph/svg_graph_.js
@@ -0,0 +1,97 @@
+const svg_graph = {
+    template: `
+		<div>
+			<h6> {{props.titre}} </h6>
+			<svg :width="props.width" :height="props.height" xmlns="http://www.w3.org/2000/svg">
+				<path :d="path" :stroke="props.color" fill="transparent" stroke-width="3"/>
+				<g>
+					<path v-for="keys in list_legends" :d="keys.path" stroke="grey" fill="transparent"/>
+
+				</g>
+			</svg>
+		</div>
+    `,
+    //{width, height, coordonates, color}
+    props:["props"],
+	data: function(){
+		return {
+			animated_coordonates: this.createPath(this.props.coordonates, this.props.width, this.props.height)
+			
+		}
+	},
+    computed: {
+        path: function(){
+			return this.animated_coordonates;
+
+        },
+		coordonates: function(){
+			return this.props.coordonates
+		},
+		list_legends: function(){
+			let res = [];
+			for(let i = 0; i< this.props.echelle_de_notation; i++){
+				res.push(null)
+			};
+			return res.map((el,i) => {return {path: `M 0 ${(this.props.height/this.props.echelle_de_notation)*(i+1)} H ${this.props.width}`, id: i}}) 
+		}
+    },
+	
+	 watch: {
+		coordonates: function(newValue){
+			gsap.to(this.$data, {duration: 0.7, animated_coordonates: this.createPath(newValue, this.props.width, this.props.height) , ease: "elastic.out(1, 0.5)"});
+		}
+	},
+    methods: {
+        createPath : (coo, maxWidth, maxHeight) => {
+		
+			let x = (el, maxW) => (el*maxW)/100;
+			let y = (el, maxH) => -((el*maxH)/100) + maxH;
+		
+			const adjust_coordonates = coo.map(el => [x(el[0], maxWidth), y(el[1], maxHeight)]);
+			
+			const line = (pointA, pointB) =>{
+				const lenX = pointB[0] - pointA[0];
+				const lenY = pointB[1] - pointA[1];
+				
+				return {
+					length : Math.sqrt(Math.pow(lenX, 2)+ Math.pow(lenX, 2)),
+					angle  : Math.atan2(lenY, lenX)
+				}
+			};
+			
+			const controlPoint = (current, previous, next, reverse) => {
+			  const p = previous || current;
+			  const n = next || current;
+			  const smoothing = 0.2;
+			  const o = line(p, n);
+			  const angle = o.angle + (reverse ? Math.PI : 0);
+			  const length = o.length * smoothing;
+			  const x = current[0] + Math.cos(angle) * length;
+			  const y = current[1] + Math.sin(angle) * length;
+			  return [x, y];
+			};
+			
+			const bezierCommand = (point, i, a) => {
+			  const [cpsX, cpsY] = controlPoint(a[i - 1], a[i - 2], point)
+			  const [cpeX, cpeY] = controlPoint(point, a[i - 1], a[i + 1], true) 
+			  
+			  return `C ${cpsX},${cpsY} ${cpeX},${cpeY} ${point[0]},${point[1]}`
+			 
+			};
+			
+			const svgPath = (points, command) => { 
+			  const d = points.reduce((acc, point, i, a) => i === 0
+				? `M ${point[0]},${point[1]}`
+				: `${acc} ${command(point, i, a)}`
+			  , '');  
+			  return d
+			};
+			
+			let final_res = svgPath(adjust_coordonates, bezierCommand)
+
+			return final_res
+		}
+    }
+}
+
+export default svg_graph
---
+++ public/javascripts/components/graph_.js
@@ -0,0 +1,100 @@
+import svg_graph from './graph/svg_graph.js'
+
+const graph = {
+    template: `
+    <div
+        :class="{'darkModeTextColor': props.darkMode, 'lightModeTextColor': props.lightMode}"
+    ><span class="hide">{{size_container}}</span>
+        <svg_graph :props="{
+            color: props.color_contrast,
+            width: size.width,
+            height: size.height,
+            coordonates: points,
+            echelle_de_notation: props.model.echelle_de_notation,
+            titre: this.props.model.items_metier[id_item].list[id_geste].nom
+        }"></svg_graph>
+    </div>
+    `,
+    props: ['props','id_geste', 'id_item', 'size_container'],
+    computed: {
+
+        points: function(){
+            const list_all_geste_by_inter = this.createNewListGeste(this.props.model.interventions);
+
+            const list = this.selectGesteById(list_all_geste_by_inter, this.id_item, this.id_geste);
+
+            const list_coordonates = this.convertListToCoordonate(list);
+
+            return list_coordonates
+        },
+
+        size: function(){
+            return { width: (this.size_container.width * 0.5), height: (this.size_container.height * 0.5) }
+        },
+        
+    },
+    methods: {
+
+        addDateToGeste: function(intervention){
+            return intervention.geste_metier.map(el => Object.assign(el, {date : intervention.date}))
+        },
+
+        createNewListGeste: function(interventions) {
+            let res = [];
+            interventions.forEach(el => {
+                res = res.concat(this.addDateToGeste(el));
+            });
+            return res
+        },
+
+        selectGesteById: function(list, id_item, id_geste){
+            return list.filter(el => el.id_geste == id_geste && el.id_item == id_item)
+        },
+
+        convertDate: function(date){
+            const array_date = date.split('/');
+            return array_date.reverse()
+        },
+
+        findMaxAndMin: function(list_of_object){
+
+            let array_of_numbers = list_of_object.map(el=> el.date_num);
+            let max = array_of_numbers.reduce((acc, cur)=> Math.max(acc, cur));
+            let min = array_of_numbers.reduce((acc, cur)=> Math.min(acc, cur));  
+            
+            return {
+                max: max,
+                min: min
+            }
+        },
+
+        addNumericDateToListObject: function(list_of_object){
+            return list_of_object.map(el => Object.assign(el, {date_num: (new Date(this.convertDate(el.date)[0], this.convertDate(el.date)[1], this.convertDate(el.date)[2] )).getTime()}));
+        },
+
+        convertListToCoordonate: function(list){
+
+            if(list.length < 2){
+
+                return [[0,0]];
+
+            } else {
+
+                let list_with_num_date = this.addNumericDateToListObject(list);
+                const max_min = this.findMaxAndMin(list_with_num_date);
+                const max_x = max_min.max;
+                const min_x = max_min.min;
+
+                let time = max_x - min_x;
+                    
+                return list_with_num_date.map(el => [(((el.date_num - min_x) / time)*100).toFixed(0) * 1,  (el.maitrise / this.props.model.echelle_de_notation) * 100])
+            }
+        }
+
+    },
+    components: {
+        svg_graph: svg_graph
+    }
+}
+
+export default graph
